5292817a5313e6986aea19f95e0e33a17641fae8
Kevin <kwa88@students.cs.ubc.ca>
2021-10-08 16:14:20 -0700

    code cleanup

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -32,10 +32,6 @@ describe("InsightFacade", function(this: Suite) {
                 expect(insightDatasets).to.be.an.instanceof(Array);
                 expect(insightDatasets).to.have.length(0);
             })
-/*
-            const futureInsightDatasets = facade.listDatasets();
-            return expect(futureInsightDatasets).to.eventually.deep.equal([]);
-            */
         });
 
         it("should list one dataset", async function() {
@@ -57,22 +53,7 @@ describe("InsightFacade", function(this: Suite) {
                     return facade.listDatasets();
                 })
                 .then((insightDatasets) => {
-                /*
-                    const expectedDatasets: InsightDataset[] = [
-                        {
-                            id: "courses",
-                            kind: InsightDatasetKind.Courses,
-                            numRows: 64612
-                        },
-                        {
-                            id: "courses-2",
-                            kind: InsightDatasetKind.Courses,
-                            numRows: 64612
-                        }
-                    ];
-                  */
                     expect(insightDatasets).to.be.an.instanceof(Array);
-                    //expect(insightDatasets).to.have.deep.members(expectedDatasets);
                     expect(insightDatasets).to.have.length(2);
                     const insightDatasetCourses = insightDatasets.find((dataset) => dataset.id === "courses");
                     expect(insightDatasetCourses).to.exist;
@@ -115,22 +96,6 @@ describe("InsightFacade", function(this: Suite) {
         })
 
         it("add dataset with duplicate id", async function() {
-        /*
-            return facade.addDataset("courses", courses, InsightDatasetKind.Courses)
-                        .then(() => {
-                            return facade.addDataset("courses", courses, InsightDatasetKind.Courses);
-                        })
-                        .then(() => {
-                            return facade.listDatasets();
-                        })
-                        .then((insightDatasets) => {
-                        console.log("insightDatasets", insightDatasets);
-                            expect(insightDatasets).to.be.an.instanceof(Array);
-                            expect(insightDatasets).to.have.length(1);
-                        })
-                        */
-            //let addedDatasets: InsightDataset[];
-
             await facade.addDataset("courses", courses, InsightDatasetKind.Courses);
 
             try {
@@ -139,12 +104,6 @@ describe("InsightFacade", function(this: Suite) {
             } catch (e) {
                 expect(e).to.be.instanceOf(InsightError);
             }
-/*
-            addedDatasets = await facade.listDatasets();
-
-            expect(addedDatasets).to.be.an.instanceof(Array);
-            expect(addedDatasets).to.have.length(1);
-            */
         })
 
         it("add text file dataset", async function() {
@@ -193,7 +152,7 @@ describe("InsightFacade", function(this: Suite) {
 
             expect(response).to.deep.equal("courses");
         })
-        //datasetId 
+
         it("Remove multiple datasets", async function() {
             try {
                 response = await facade.removeDataset("courses");
@@ -355,80 +314,3 @@ describe("InsightFacade", function(this: Suite) {
         })
     })
 });
-
-
-
-
-
-/*
-describe('addDataset', function() {
-    let insightFacade: InsightFacade;
-    let classes: string;
-    let classesZip = new JSZip();
-    const content: string[] = ["foo", "bar"];
-
-    before(function() {
-        insightFacade = new InsightFacade();
-        //classes = JSON.parse(fs.readFileSync("./test/data/courses.zip", 'utf8'));
-        classes = fs.readFileSync("./test/data/courses.zip").toString('base64');
-
-        /*
-        fs.readFile("./test/data/courses.zip", function(err, data) {
-            if (err) throw err;
-            classesZip.loadAsync(data).then(function(zip) {
-                zip.forEach(function (path, file) {
-                    classes.push(btoa(JSON.stringify(file)));
-                })
-            })
-            console.log(classes);
-        })
-
-    })
-
-    it("add valid dataset", async() => {
-        const id: string = "foo";
-        let response: string[];
-
-            content.forEach(function (val) {
-                insightFacade.addDataset(id, val, InsightDatasetKind.Courses).then(function (response) {
-                    console.log(response);
-                }).catch(function(e) {
-                    assert.fail("addDataset fail");
-                });
-            })
-            //response = await insightFacade.addDataset(id, content, InsightDatasetKind.Courses)
-    })
-
-    it("add dataset with underscore id", async() => {
-        const id: string = "foo_";
-        let error: string = "";
-
-        content.forEach(function (val) {
-            insightFacade.addDataset(id, val, InsightDatasetKind.Courses).then(function (response) {
-                console.log(response);
-            }).catch(function(e) {
-                error = "error";
-            });
-        })
-
-        assert.equal(error, "error");
-    })
-
-    it("add dataset with whitespace id", async() => {
-        const id: string = " ";
-        let error: string = "";
-
-        content.forEach(function (val) {
-            insightFacade.addDataset(id, val, InsightDatasetKind.Courses).then(function (response) {
-                console.log(response);
-            }).catch(function(e) {
-                error = "error";
-            });
-        })
-
-        assert.equal(error, "error");
-    })
-
-
-});
-*/
