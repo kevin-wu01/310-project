f5c88bc3ba38a6f116283494a7f61e50b7724a3e
iPoutine <58972848+iPoutine@users.noreply.github.com>
2021-10-09 11:25:10 -0700

    push current changes

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -6,11 +6,10 @@ import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFou
  *
  */
 export default class InsightFacade implements IInsightFacade {
-	/*
 	constructor() {
 		console.trace("InsightFacadeImpl::init()");
 	}
-*/
+
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		return Promise.resolve([]);
 	}
@@ -20,7 +19,30 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public performQuery(query: any): Promise<any[]> {
-		return Promise.reject("Not implemented.");
+		const where: Record<string, any> = query.WHERE;
+		const options: Record<string, any> = query.OPTIONS;
+
+		let queryString: string = Object.keys(where)[0];
+
+		if (!queryString) {
+			throw new InsightError();
+		}
+
+		switch(queryString) {
+		case "LT":
+			break;
+		case "GT":
+			break;
+		case "EQ":
+			break;
+		case "AND":
+			break;
+		case "OR":
+			break;
+		default: throw new InsightError();
+		}
+
+		return Promise.resolve([]);
 	}
 
 	public listDatasets(): Promise<InsightDataset[]> {
