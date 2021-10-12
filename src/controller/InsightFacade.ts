4f618ab564db582ff143315409aee097e17f4f54
iPoutine <58972848+iPoutine@users.noreply.github.com>
2021-10-12 12:37:10 -0700

    push current progress

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,4 +1,5 @@
-import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
+import {IInsightFacade, InsightDataset, InsightDatasetKind,
+	InsightError, NotFoundError, ResultTooLargeError} from "./IInsightFacade";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -6,6 +7,7 @@ import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFou
  *
  */
 export default class InsightFacade implements IInsightFacade {
+
 	constructor() {
 		console.trace("InsightFacadeImpl::init()");
 	}
@@ -22,50 +24,158 @@ export default class InsightFacade implements IInsightFacade {
 		const where: Record<string, any> = query.WHERE;
 		const options: Record<string, any> = query.OPTIONS;
 
-		let queryString: string = Object.keys(where)[0];
+		let filteredData: any[] = this.filterData([], where);
 
-		let filteredData: any[] = this.filterData(queryString);
-
-		return Promise.resolve([]);
-	}
-
-	private filterData(query: any): any[] {
-		if (!query) {
-			//return array of data
+		if (filteredData.length > 5000) {
+			throw new ResultTooLargeError();
 		}
 
-		let nextQuery: string[] = Object.keys(query);
+		filteredData = this.filterOptions(filteredData, options);
 
-		switch(query) {
+		return Promise.resolve(filteredData);
+	}
+
+	private filterData(data: any[], query: any): any[] {
+		const queryString: string = Object.keys(query)[0];
+
+		if (!queryString) {
+			return data;
+		}
+
+		switch(queryString) {
 		case "LT":
-			//lower than 
+			this.checkValidMComparator(query);
+			data = this.filterMComparator(data, "LT", Object.keys(query.LT)[0], query[Object.keys(query.LT)[0]]);
+			// lower than
 			break;
 		case "GT":
-			//greater than
+			this.checkValidMComparator(query);
+			data = this.filterMComparator(data, "GT", Object.keys(query.GT)[0], query[Object.keys(query.LT)[0]]);
+			// greater than
 			break;
 		case "EQ":
-			//equal to 
+			this.checkValidMComparator(query);
+			data = this.filterMComparator(data, "EQ", Object.keys(query.EQ)[0], query[Object.keys(query.LT)[0]]);
+			// equal to
 			break;
 		case "AND":
-			//intersection of recursion 
-			this.filterAND(nextQuery);
+			// intersection of recursion
+			data = this.filterAND(data, query.AND);
 			break;
 		case "OR":
-			//union of recursion
-			this.filterOR(nextQuery);
+			// union of recursion
+			data = this.filterOR(data, query.OR);
+			break;
+		case "IS":
+			break;
+		case "NOT":
 			break;
 		default: throw new InsightError();
 		}
 
-		return [];
+		return data;
 	}
 
-	private filterAND(query: any): any[] {
-		return [];
+	private filterAND(data: any[], queryArray: any): any[] {
+		let queryResults: any[] = [];
+
+		for (let query of queryArray) {
+			queryResults.push(this.filterData(data, query));
+		}
+
+		queryResults = queryResults.reduce((a, b) => a.filter((c: any) => b.includes(c))); // intersection of multiple arrays
+
+		return queryResults;
 	}
 
-	private filterOR(query: any): any[] {
-		return [];
+	private filterOR(data: any[], queryArray: any): any[] {
+		let queryResults: any[] = [];
+
+		for (let query of queryArray) {
+			queryResults.push(this.filterData(data, query));
+		}
+
+		queryResults = queryResults.filter((classData, index, self) => {
+			return index === self.findIndex((t) => {
+				return t.name === classData.name; // replace with data properties
+			});
+		});
+
+
+		return queryResults;
+	}
+
+	private checkValidMComparator(query: any): void {
+		if (Object.keys(query).length > 1 || Object.keys(query).length === 0) {
+			throw new InsightError();
+		}
+
+		if (typeof query[Object.keys(query)[0]] !== "number") {
+			throw new InsightError();
+		}
+	}
+
+	private filterMComparator(data: any[], comparator: string, field: string, value: number): any[] {
+		let filteredData: any[] = [];
+
+		switch(comparator) {
+		case "LT":
+			filteredData = data.filter((dataClass) => {
+				return dataClass[field] < value;
+			});
+			break;
+		case "GT":
+			filteredData = data.filter((dataClass) => {
+				return dataClass[field] > value;
+			});
+			break;
+		case "EQ":
+			filteredData = data.filter((dataClass) => {
+				return dataClass[field] === value;
+			});
+			break;
+		}
+
+		return filteredData;
+	}
+
+	private filterOptions(data: any[], query: any): any[] {
+		const dataColumns: string[] = query.COLUMNS;
+		const sortColumn: string = query.ORDER.split("_")[1];
+		let filteredData: any[] = [];
+
+		if (dataColumns.length === 0) {
+			throw new InsightError();
+		}
+
+		this.checkValidKey(sortColumn);
+		for (let key of dataColumns) {
+			this.checkValidKey(key.split("_")[1]);
+		}
+
+		for (let section = 0; section < data.length; section++) {
+			filteredData.push({});
+
+			for (let key of dataColumns) {
+				let substrKey: string = key.split("_")[1];
+				filteredData[section][substrKey] = data[section][substrKey];
+			}
+		}
+
+		filteredData.sort((a,b) => {
+			return a[sortColumn] - b[sortColumn];
+		});
+
+		return filteredData;
+	}
+
+	private checkValidKey(key: string): void {
+		const validKeys: string[] = ["avg", "pass", "fail", "audit", "year",
+			"dept", "id", "instructor", "title", "uuid"];
+
+		if (!validKeys.includes(key)) {
+			throw new InsightError();
+		}
 	}
 
 	public listDatasets(): Promise<InsightDataset[]> {
