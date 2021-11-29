33eab160f5981e2d3bee9ce4952e347a39df0bd0
Kevin <kwa88@students.cs.ubc.ca>
2021-11-28 17:44:05 -0800

    add tests, comment out default datasets

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -5,6 +5,49 @@ import chai, {expect, use} from "chai";
 import chaiHttp from "chai-http";
 import * as fs from "fs";
 
+const query = {
+	WHERE: {
+		AND: [
+			{
+				IS: {
+					courses_dept: "*cps*"
+				}
+			},
+			{
+				GT: {
+					courses_avg: 92
+				}
+			}
+		]
+	},
+	OPTIONS: {
+		COLUMNS: [
+			"courses_avg",
+			"courses_dept",
+			"maxAvg"
+		],
+		ORDER: {
+			dir: "DOWN",
+			keys: [
+				"maxAvg"
+			]
+		}
+	},
+	TRANSFORMATIONS: {
+		GROUP: [
+			"courses_dept",
+			"courses_avg"
+		],
+		APPLY: [
+			{
+				maxAvg: {
+					MAX: "courses_avg"
+				}
+			}
+		]
+	}
+};
+
 describe("Facade D3", function () {
 
 	let facade: InsightFacade;
@@ -16,10 +59,12 @@ describe("Facade D3", function () {
 		facade = new InsightFacade();
 		server = new Server(4321);
 		// TODO: start server here once and handle errors properly
+		server.start();
 	});
 
 	after(function () {
 		// TODO: stop server here once!
+		server.stop();
 	});
 
 	beforeEach(function () {
@@ -34,7 +79,7 @@ describe("Facade D3", function () {
 
 	it("PUT test for courses dataset", function () {
 		const SERVER_URL = "http://localhost:4321";
-		const ENDPOINT_URL = "/datasets/foo/courses";
+		const ENDPOINT_URL = "/datasets/courses/courses";
 		const ZIP_FILE_DATA = fs.readFileSync("./test/data/courses.zip");
 
 		try {
@@ -44,6 +89,7 @@ describe("Facade D3", function () {
 				.set("Content-Type", "application/x-zip-compressed")
 				.then(function (res) {
 					// some logging here please!
+					console.log(res.body, "res");
 					expect(res.status).to.be.equal(200);
 				})
 				.catch(function (err) {
@@ -64,8 +110,80 @@ describe("Facade D3", function () {
 		try {
 			return chai.request(SERVER_URL)
 				.get(ENDPOINT_URL)
+				.set("Content-Type", "application/json")
 				.then(function (res) {
 					// some logging here please!
+					console.log(res.body, "res");
+					expect(res.status).to.be.equal(200);
+				})
+				.catch(function (err) {
+					// some logging here please!
+					console.log(err, "err1");
+					expect.fail();
+				});
+		} catch (err) {
+			console.log(err, "err2");
+			// and some more logging here!
+		}
+	});
+
+	it("DELETE dataset", function () {
+		const SERVER_URL = "http://localhost:4321";
+		let ENDPOINT_URL = "/datasets/ubc";
+
+		try {
+			chai.request(SERVER_URL)
+				.delete(ENDPOINT_URL)
+				.set("Content-Type", "application/json")
+				.then(function (res) {
+					// some logging here please!
+					console.log(res.body, "res");
+					expect(res.status).to.be.equal(200);
+				})
+				.catch(function (err) {
+					// some logging here please!
+					console.log(err, "err1");
+					expect.fail();
+				});
+		} catch (err) {
+			console.log(err, "err2");
+			// and some more logging here!
+		}
+
+		ENDPOINT_URL = "/datasets";
+
+		try {
+			return chai.request(SERVER_URL)
+				.get(ENDPOINT_URL)
+				.set("Content-Type", "application/json")
+				.then(function (res) {
+					// some logging here please!
+					console.log(res.body, "res");
+					expect(res.status).to.be.equal(200);
+				})
+				.catch(function (err) {
+					// some logging here please!
+					console.log(err, "err1");
+					expect.fail();
+				});
+		} catch (err) {
+			console.log(err, "err2");
+			// and some more logging here!
+		}
+	});
+
+	it("POST query", function () {
+		const SERVER_URL = "http://localhost:4321";
+		const ENDPOINT_URL = "/query";
+
+		try {
+			return chai.request(SERVER_URL)
+				.post(ENDPOINT_URL)
+				.send(query)
+				.set("Content-Type", "application/json")
+				.then(function (res) {
+					// some logging here please!
+					console.log(res.body, "res");
 					expect(res.status).to.be.equal(200);
 				})
 				.catch(function (err) {
